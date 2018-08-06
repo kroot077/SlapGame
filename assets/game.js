a08100fc53bb57c1934c07fd50fe463b1db54442
Keith Root <hazelroot11@gmail.com>
2018-08-06 03:35:07 -0600

    Want a unique mechanic? Better work hard for it

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,6 @@
 let player = {
     health: 50,
-    specialty: 10,
+    specialty: 25,
     guard: 15,
     attack: 3
 }
@@ -31,12 +31,13 @@ plSpecialty.innerText = player.specialty.toString();
 amount.innerText = item.amount.toString();
 
 function cycle() {
-    if (player.health <= 0 || enemy.health <=0) {
+    if (player.health <= 0 || enemy.health <= 0) {
         document.getElementById('slap').disabled = true;
         document.getElementById('special').disabled = true;
         document.getElementById('item').disabled = true;
         document.getElementById('guard').disabled = true;
     } else {
+        document.getElementById('hits').innerText = hit;
         document.getElementById('en-health').innerText = enemy.health;
         document.getElementById('pl-health').innerText = player.health;
         document.getElementById('pl-guard').innerText = player.guard;
@@ -53,27 +54,35 @@ function slap() {
 }
 
 function specialAct() {
-    var sp = enemy.health - Math.floor(player.attack * Math.floor(Math.random() * (4 - 2)));
-    if (player.specialty <= 0) {
-        document.getElementById('special').disabled = true;
+    var sp = enemy.health - Math.floor(player.attack * Math.floor(Math.random() * (4 - 2) + 2));
+    if (player.specialty >= 0) {
+        enemy.health = sp;
+        player.specialty = player.specialty - 5;
+        hit ++;       
+    } 
+    if (player.specialty == 0) {
+    document.getElementById('special').disabled = true;
     }
-    enemy.health = sp;
-    hit ++;
     cycle();
 }
 
 function plItem() {
     var item = player.attack * item.modifier;
-    if (item.amount <= 0) {
-        document.getElementById('item').disabled = true;
-    } else {
-    item.amount --;
-    player.attack = item; 
+    if (item.amount >= 0) {
+        item.amount = item.amount - 1;
+        player.attack = item; 
+    } 
+    if (item.amount == 0) {
+    document.getElementById('item').disabled = true;
     }
     cycle();
 }
 
 function guard() {
+    var block = player.guard - enemy.attacks
+    if (player.guard >= 0) {
+        player.guard
+    }
     if (player.guard == 0) {
         document.getElementById('guard').disabled = true;
     }
