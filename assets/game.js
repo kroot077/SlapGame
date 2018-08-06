c1f29326dce4840c74b95f1b1a4d27d1161f3cb2
Keith Root <hazelroot11@gmail.com>
2018-08-06 02:45:02 -0600

    building

--- assets/game.js
+++ assets/game.js
@@ -1,53 +1,77 @@
-function GameService () {
-
-}
-
 let player = {
     health: 50,
-    specialty: 50,
-    guard: 50,
-    attack: 3,
+    specialty: 10,
+    guard: 15,
+    attack: 3
+}
+
+let item = {
+    modifier: 1.5,
+    amount: 2
 }
 
 let enemy = {
     health: 50,
-    specialty: 50,
-    guard: 50,
+    specialty: 10,
+    guard: 15,
     attacks: ['slap1','slap2','slap3','parry1','parry2','mean eye','pump iron','insult'],
+    appearance: [
+        'dandy-man-vintage-image-GraphicsFairy.jpg',
+    ]
 }
-
-let enemyIndex = ['slap1','slap2','slap3','parry1','parry2','mean eye','pump iron','insult']
-let enHealth1 = 50;
 
 let actTurn = 0;
 
-let spclIndex = ['backhand', 'palm', 'pinch nose']
-let itemIndex = ['muscle tonic']
+let enHealth = document.getElementById('en-health')
+let plHealth = document.getElementById('pl-health')
+let plGuard = document.getElementById('pl-guard')
+let amount = document.getElementById('amount')
+let plSpecialty = document.getElementById('special-bar')
 
-document.getElementById('enHealth').innerText = player.health.toString()
+enHealth.innerText = enemy.health.toString();
+plHealth.innerText = player.health.toString();
+plGuard.innerText = player.guard.toString();
+plSpecialty.innerText = player.specialty.toString();
 
-let enHealth = document.getElementById('enHealth')
-let plHealth = document.getElementById('plHealth')
-const slct1 = document.getElementById('slap')
-const slct2 = document.getElementById('specialty')
-const slct3 = document.getElementById('items')
-const slct4 = document.getElementById('guard')
+amount.innerText = item.amount.toString();
 
-function values () {
-    enHealth.innerText = enemy.health.toString();
-    plHealth.innerText = player.health.toString();
+function draw() {
+    
 }
 
 function slap() {
+    var attack = enemy.health - player.attack;
+    if (enemy.attack[3] || enemy.attack[4]) {
+        enemy.guard - player.attack;
+    }
+    enemy.health = attack;
+    enHealth.innerText = enemy.health.toString();
+    draw();
+}
+
+function specialAct() {
     if (enemy.attack[3] || enemy.attack[4]) {
         enemy.health - Math.floor(player.attack * 0.75);
     }
-    enemy.health - player.attack;
+    enemy.health - Math.floor(player.attack * Math.floor(Math.random() * (4 - 2)));
+    draw();
 }
 
-if (enemy.health == 0 || player.health == 0) {
-    slct1.disabled = true;
-    slct2.disabled = true;
-    slct3.disabled = true;
-    slct4.disabled = true;
-} 
+function plItem() {
+    var item = player.attack * item.modifier;
+    if (item.amount == 0) {
+        item.amount = item.amount + 0;
+    } else {
+    item.amount = item.amount --;
+    player.attack = item; 
+    }
+    draw();
+}
+
+function guard() {
+    if (player.guard = 0) {
+        return null;
+    }
+    var block = player.guard - enemy.attack
+    draw();
+}
